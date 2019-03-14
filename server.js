d566a5e1fc13111fb675bf679b9127dbdcdba24f
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-03-14 15:19:38 -0700

    Adds proxy for all microservices

--- server.js
+++ server.js
@@ -14,22 +14,24 @@ app.use(morgan('dev'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/menu/bundle', proxy({
-  target: 'http://localhost:3031/'
+  target: 'http://ec2-18-223-135-231.us-east-2.compute.amazonaws.com'
 }));
 
 app.use('/api/:restaurantId/menu', proxy({
-  target: 'http://localhost:3031/'
+  target: 'http://ec2-18-223-135-231.us-east-2.compute.amazonaws.com'
 }));
 
-// app.use('/api/photos', proxy({
-//   target: 'http://ec2-18-206-121-61.compute-1.amazonaws.com'
-// }));
-// app.use('/api/reserve', proxy({
-//   target: 'http://ec2-18-191-229-0.us-east-2.compute.amazonaws.com/'
-// }));
-// app.use('/overview', proxy({
-//   target: 'http://ec2-18-191-13-163.us-east-2.compute.amazonaws.com/'
-// }));
+app.use('/api/photos', proxy({
+  target: 'http://ec2-18-222-149-148.us-east-2.compute.amazonaws.com'
+}));
+
+app.use('/api/reserve', proxy({
+  target: 'http://ec2-18-222-239-157.us-east-2.compute.amazonaws.com'
+}));
+
+app.use('/overview', proxy({
+  target: 'http://ec2-3-92-162-226.compute-1.amazonaws.com/'
+}));
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 
